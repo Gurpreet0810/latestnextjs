cb7888668fc7256ebe07e812d676c98b2c85415a
Gurpreet Singh <1221320gurpreet@gmail.com>
2024-11-09 17:10:04 +0530

    new evening

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -29,17 +29,17 @@ const Header = () => {
 
   return (
     <header
-      className={` left-0 top-5 z-99999 w-full border-t border-b border-black border-solid ${
+      className={`absolute left-0 top-5 z-99999 w-full  ${
         stickyMenu
           ? "bg-white !py-4 shadow transition duration-100 dark:bg-black"
           : ""
       }`}
     >
-      <div className="relative mx-auto max-w-c-1390 items-center justify-between px-4 md:px-8 xl:flex 2xl:px-0">
+      <div className="relative mx-auto max-w-[86.875rem]  border-t border-b border-white border-solid items-center justify-between px-4 md:px-8 xl:flex 2xl:px-0">
         <div className="flex w-full items-center justify-between xl:w-1/4">
           <a href="/">
             <Image
-              src="/logo.png"
+              src="/1 1.png"
               alt="logo"
               width={50.03}
               height={30}
@@ -96,9 +96,9 @@ const Header = () => {
           }`}
         >
           <nav>
-            <ul className="flex flex-col gap-5 xl:flex-row xl:items-center xl:gap-10">
+            <ul className="flex flex-col gap-5 xl:flex-row !text-white xl:items-center xl:gap-10">
               {menuData.map((menuItem, key) => (
-                <li key={key} className={menuItem.submenu && "group relative"}>
+                <li key={key} className={menuItem.submenu && "group relative "}>
                   {menuItem.submenu ? (
                     <>
                       <button
@@ -108,7 +108,7 @@ const Header = () => {
                         {menuItem.title}
                         <span>
                           <svg
-                            className="h-3 w-3 cursor-pointer fill-waterloo group-hover:fill-primary"
+                            className="h-3 w-3 cursor-pointer fill: white fill-waterloo"
                             xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 512 512"
                           >
@@ -118,10 +118,10 @@ const Header = () => {
                       </button>
 
                       <ul
-                        className={`dropdown ${dropdownToggler ? "flex z-max" : ""}`}
+                        className={`dropdown ${dropdownToggler ? "flex z-99999" : ""}`}
                       >
                         {menuItem.submenu.map((item, key) => (
-                          <li key={key} className="hover:text-primary">
+                          <li key={key} className="text-black ">
                             <Link href={item.path || "#"}>{item.title}</Link>
                           </li>
                         ))}
@@ -132,8 +132,8 @@ const Header = () => {
                       href={`${menuItem.path}`}
                       className={
                         pathUrl === menuItem.path
-                          ? "text-primary hover:text-primary"
-                          : "hover:text-primary"
+                          ? "text-white"
+                          : ""
                       }
                     >
                       {menuItem.title}
@@ -148,24 +148,43 @@ const Header = () => {
           <div className="px-4 py-6 md:flex md:items-center md:justify-between">
             <div className="flex mt-4 sm:justify-center md:mt-0 space-x-5 rtl:space-x-reverse">
               <a href="#" className="text-gray-400 hover:text-gray-900 dark:hover:text-white">
-              <img src="/24.png" className="" alt="Flowbite Logo" />
+              {/* <img src="/24.png" className="" alt="Flowbite Logo" /> */}
                 <span className="sr-only">Facebook page</span>
+                <svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M8.78609 9.33674H6.20974V17.5634H2.39045V9.33674H0.573242V6.11862H2.39045V4.03562C2.39045 2.54554 3.10239 0.212891 6.24514 0.212891L9.07716 0.224593V3.34909H7.02395C6.68568 3.34909 6.21368 3.51682 6.21368 4.22676V6.12252H9.12436L8.79003 9.34064L8.78609 9.33674Z" fill="white"/>
+</svg>
+
               </a>
 
               {/* LinkedIn Icon */}
               <a href="#" className="text-blue-700 hover:text-blue-900">
-              <img src="/22.png" className="" alt="Flowbite Logo" />
-                <span className="sr-only">LinkedIn</span>
+              <svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M16.0087 9.34546V14.9586H12.7283V9.71993C12.7283 8.40538 12.2563 7.5082 11.0645 7.5082C10.1559 7.5082 9.61704 8.11282 9.38104 8.69793C9.2945 8.90857 9.2709 9.19723 9.2709 9.48979V14.9586H5.98655C5.98655 14.9586 6.02981 6.08833 5.98655 5.16776H9.2709V6.55642C9.2709 6.55642 9.25517 6.57983 9.2473 6.58763H9.2709V6.55642C9.70751 5.88939 10.4863 4.93761 12.2288 4.93761C14.3882 4.93761 16.0087 6.33798 16.0087 9.34156V9.34546ZM0.892853 14.9586H4.17327V5.16776H0.892853V14.9586ZM4.27161 1.84822C4.27161 2.7727 3.51639 3.52554 2.58026 3.52554C1.64412 3.52554 0.888916 2.7766 0.888916 1.84822C0.888916 0.919842 1.64412 0.170898 2.58026 0.170898C3.51639 0.170898 4.27161 0.919842 4.27161 1.84822Z" fill="white"/>
+</svg>
+
               </a>
 
-              {/* Instagram Icon */}
               <a href="#" className="text-pink-500 hover:text-pink-700">
-              <img src="/23.png" className="" alt="Flowbite Logo" />
-                <span className="sr-only">Instagram</span>
+              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="17" height="16" viewBox="0 0 56.7 56.7" fill="white">
+  <g>
+    <path d="M28.2,16.7c-7,0-12.8,5.7-12.8,12.8s5.7,12.8,12.8,12.8S41,36.5,41,29.5S35.2,16.7,28.2,16.7z M28.2,37.7
+      c-4.5,0-8.2-3.7-8.2-8.2s3.7-8.2,8.2-8.2s8.2,3.7,8.2,8.2S32.7,37.7,28.2,37.7z"/>
+    <circle cx="41.5" cy="16.4" r="2.9"/>
+    <path d="M49,8.9c-2.6-2.7-6.3-4.1-10.5-4.1H17.9c-8.7,0-14.5,5.8-14.5,14.5v20.5c0,4.3,1.4,8,4.2,10.7c2.7,2.6,6.3,3.9,10.4,3.9
+      h20.4c4.3,0,7.9-1.4,10.5-3.9c2.7-2.6,4.1-6.3,4.1-10.6V19.3C53,15.1,51.6,11.5,49,8.9z M48.6,39.9c0,3.1-1.1,5.6-2.9,7.3
+      s-4.3,2.6-7.3,2.6H18c-3,0-5.5-0.9-7.3-2.6C8.9,45.4,8,42.9,8,39.8V19.3c0-3,0.9-5.5,2.7-7.3c1.7-1.7,4.3-2.6,7.3-2.6h20.6
+      c3,0,5.5,0.9,7.3,2.7c1.7,1.8,2.7,4.3,2.7,7.2V39.9L48.6,39.9z"/>
+  </g>
+</svg>
+
+
+
+
+
               </a>
             </div>
           </div>
-          <button type="button" className="text-black border-black  border border-solid  bg-white-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm m-[14px] px-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          <button type="button" className="text-white border-white  border border-solid  bg-white-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm m-[14px] px-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Book
           </button>
           <button type="button" className="  m-[13px] text-black bg-[#A1CF5F] hover:bg-[#A1CF5F]/90 focus:ring-4 focus:outline-none focus:ring-[#A1CF5F]/50 font-medium rounded-lg text-sm px-4 py-2 text-center dark:focus:ring-[#A1CF5F]/70">
